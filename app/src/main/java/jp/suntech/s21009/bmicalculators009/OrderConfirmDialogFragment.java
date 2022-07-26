665a536ccf6ad53d7fb35606f191e4b546d7e652
admin <s21009@suntech.jp>
2022-07-26 11:37:19 +0900

    Second Commit

---
+++ app/src/main/java/jp/suntech/s21009/bmicalculators009/OrderConfirmDialogFragment.java
@@ -0,0 +1,31 @@
+package jp.suntech.s21009.bmicalculators009;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.FragmentManager;
+
+public class OrderConfirmDialogFragment extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState){
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        builder.setTitle(R.string.dialog_title);
+        builder.setMessage(R.string.dialog_msg);
+        builder.setPositiveButton(R.string.dialog_btn_ok,new DialogButtonClickListener());
+
+        AlertDialog dialog = builder.create();
+        return dialog;
+    }
+
+    private  class DialogButtonClickListener implements DialogInterface.OnClickListener{
+        @Override
+        public void onClick(DialogInterface dialog, int which){
+
+        }
+    }
+}
